facbc7a63915f09374e59822b3453adf55b0ac85
thinhdh04 <tdh855513@gmail.com>
2025-01-04 00:11:47 +0700

    vowel counter

---
+++ Project-10-Vowel-Counter/app.js
@@ -0,0 +1,23 @@
+function countVowels(){
+    var text = document.getElementById("inputText").value; // hello
+    var vowelCount = 0;
+
+    text = text.toLowerCase();
+
+    for(var i = 0; i < text.length; i++){
+        var char = text.charAt(i);
+        if(isVowel(char)){
+            vowelCount++;
+        }
+
+    }
+
+    var result = document.getElementById("result");
+    result.textContent = "Total Vowels: " + vowelCount;
+
+}
+
+function isVowel(char){
+    var vowels = ["a", "e", "i", "o", "u"];
+    return vowels.includes(char);
+}
